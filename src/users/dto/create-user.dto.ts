f02a877f164246308993ea43ea4391aa9bb0930d
a-badi <ali.abdi.1991@gmail.com>
2023-01-29 18:59:38 +0330

    add user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    email: string;
+
+    password: string;
+
+    passwordConfirm: string;
+
+    credit: string;
+
+    profilePicture: string
+
+    phoneNumber: string[];
+}
