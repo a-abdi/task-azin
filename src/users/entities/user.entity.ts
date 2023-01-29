fb14020591b80025128be1b429a1c73cf8f96b90
a-badi <ali.abdi.1991@gmail.com>
2023-01-29 18:54:24 +0330

    add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Schema as MongooseSchema } from "mongoose";
+
+export class User {
+    _id: MongooseSchema.Types.ObjectId;
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    password: string;
+
+    @Prop()
+    credit: string;
+
+    @Prop()
+    profilePicture: string
+
+    @Prop()
+    phoneNumber: string[];
+}
